8ee6f26bbd32dde2b627e6aa4c7d57e77274cc41
cd-dist-2324 <cd.dist.2324@gmail.com>
2024-06-11 15:46:26 +0200

    prueba r-ud2 commit enunciado

--- src/figuras/Circulo.java
+++ src/figuras/Circulo.java
@@ -13,13 +13,6 @@ public class Circulo extends FiguraGeometrica {
 		super();
 	}
 	
-	public double getRadio() {
-		return radio;
-	}
-
-	public void setRadio(double radio) {
-		this.radio = radio;
-	}
 
 	public Circulo(double radio){
 		super();
